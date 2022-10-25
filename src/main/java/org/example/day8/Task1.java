32513d9c8593700379733c2f6d28f31c944ef040
Abolior <dimamatveev121@gmail.com>
2022-10-25 22:19:07 +0500

    string bulder

---
+++ src/main/java/org/example/day8/Task1.java
@@ -0,0 +1,7 @@
+package org.example.day8;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/day8/Traint.java
@@ -0,0 +1,17 @@
+package org.example.day8;
+
+public class Traint {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+
+        System.out.printf("Stirng %10s \n", "10000");
+        System.out.printf("Stirng %10s \n", "1000");
+        System.out.printf("Stirng %10d \n", 100);
+        System.out.printf("Stirng %.3f \n", 100.328768768);
+        System.out.printf("Stirng %.2f \n", 100.14563456);
+        System.out.printf("Stirng %.2f \n", 100.3723);
+        System.out.printf("Stirng %.4f \n", 100.3213123);
+        System.out.printf("Stirng %.3f \n", 100.3123);
+        // % [10][.32]
+    }
+}
