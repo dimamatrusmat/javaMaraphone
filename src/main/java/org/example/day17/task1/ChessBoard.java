391f5046b29d56d645648e5b54dc6e380aff15e5
Abolior <dimamatveev121@gmail.com>
2022-11-01 21:44:53 +0500

    all se

--- src/main/java/org/example/day17/task1/ChessBoard.java
+++ src/main/java/org/example/day17/task1/ChessBoard.java
@@ -1,33 +1,20 @@
 package org.example.day17.task1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-
 public class ChessBoard {
     private StringBuilder sb = new StringBuilder("");
 
-    public ChessBoard(ChessPiece [][] coordies) throws FileNotFoundException {
-        File file = new File("Chees.txt");
-        PrintWriter pr = new PrintWriter(file);
+    public ChessBoard(ChessPiece [][] coordies){
 
         for (ChessPiece [] cp: coordies) {
             for (ChessPiece c: cp) {
-//                sb.append(c.getFigure());
-                pr.print(c.getScil());
+                sb.append(c.getFigure());
             }
-            pr.println("");
-//            sb.append("\n");
+            sb.append("\n");
         }
-        pr.close();
-//        for (int i = 0; i < 8; i++) {
-//            for (int o = 0; o < 8; o++) {
-//
-//            }
-//        }
+
     }
 
     public void print() {
-        System.out.println(sb);
+        System.out.printf("%S",sb);
     }
 }
