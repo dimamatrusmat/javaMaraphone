aae71e6e36b1d3683fb0538ae51890f48cca4e62
Abolior <dimamatveev121@gmail.com>
2022-11-01 21:07:01 +0500

    yuar

--- src/main/java/org/example/day17/task1/ChessBoard.java
+++ src/main/java/org/example/day17/task1/ChessBoard.java
@@ -1,16 +1,30 @@
 package org.example.day17.task1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class ChessBoard {
     private StringBuilder sb = new StringBuilder("");
 
-    public ChessBoard(float[][] floats) {
+    public ChessBoard(ChessPiece [][] coordies) throws FileNotFoundException {
+        File file = new File("Chees.txt");
+        PrintWriter pr = new PrintWriter(file);
 
-        for (int i = 0; i < 8; i++) {
-            for (int o = 0; o < 8; o++) {
-                sb.append(ChessPiece.EMPTY);
+        for (ChessPiece [] cp: coordies) {
+            for (ChessPiece c: cp) {
+//                sb.append(c.getFigure());
+                pr.print(c.getScil());
             }
-            sb.append("\n");
+            pr.println("");
+//            sb.append("\n");
         }
+        pr.close();
+//        for (int i = 0; i < 8; i++) {
+//            for (int o = 0; o < 8; o++) {
+//
+//            }
+//        }
     }
 
     public void print() {
--- src/main/java/org/example/day17/task1/Task1.java
+++ src/main/java/org/example/day17/task1/Task1.java
@@ -1,15 +1,25 @@
 package org.example.day17.task1;
 
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 //        System.out.println(ChessPiece.PAWN_WHINE);
-        ChessBoard chessBoard = new ChessBoard(new float[][]{});
-//        chessBoard.print();
-        String [][] coordies = new String[][]{
-          {3.5f,-1,-1,-1,-1,-3.5f,100,-1},
-          {-1, ,-1,-1,,,-1,},
+        ChessPiece [][] coordies = new ChessPiece[][]{
+            {ChessPiece.ROOK_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK,ChessPiece.EMPTY},
+            {ChessPiece.EMPTY, ChessPiece.ROOK_WHINE, ChessPiece.EMPTY,ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK},
+            {ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.KING_BLACK, ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY},
+            {ChessPiece.QUEEN_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHINE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY,},
+            {ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY, ChessPiece.BISHOP_BLACK, ChessPiece.PAWN_WHINE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY,},
+            {ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY, ChessPiece.BISHOP_WHINE, ChessPiece.PAWN_WHINE, ChessPiece.EMPTY, ChessPiece.EMPTY,},
+            {ChessPiece.PAWN_WHINE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.QUEEN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHINE, ChessPiece.EMPTY, ChessPiece.PAWN_WHINE},
+            {ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY,ChessPiece.EMPTY, ChessPiece.ROOK_WHINE, ChessPiece.KING_WHITE, ChessPiece.EMPTY,},
         };
+
+        ChessBoard chessBoard = new ChessBoard(coordies);
+        chessBoard.print();
+
+
     }
 }
